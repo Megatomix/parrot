b3efaea65697ad8018c14e0e11fb94e6ac5f3cf5
Gustavo Aguiar <gugahoa@gmail.com>
2018-03-29 20:01:40 +0000

    Send events to aligator and integration

---
+++ apps/shooter/config/dev.exs
@@ -0,0 +1,4 @@
+use Mix.Config
+
+config :shooter,
+  aligator_url: "https://localhost:4000"
--- apps/shooter/lib/shooter.ex
+++ apps/shooter/lib/shooter.ex
@@ -3,14 +3,42 @@ defmodule Shooter do
 
   def shoot_msg(%{"app_id" => app_id} = payload) do
     Logger.debug "Shooting #{inspect payload}"
-    integration = Parrot.Customers.get_integration(app_id)
-    HTTPoison.post(
-      integration.integration_endpoint,
-      payload |> Poison.encode!,
-      [
-        {"Content-Type", "application/json"},
-      ]
-    )
+    send_to_integration(app_id, payload)
+    send_to_aligator(payload)
   end
   def shoot_msg(_payload), do: nil
+
+  defp send_to_integration(app_id, payload) do
+    integration = Parrot.Customers.get_integration(app_id)
+    if integration != nil do
+      HTTPoison.post(
+        integration.integration_endpoint,
+        payload |> Poison.encode!,
+        [
+          {"Content-Type", "application/json"},
+        ]
+      )
+    else
+      nil
+    end
+  end
+
+  defp send_to_aligator(payload) do
+    if aligator_url() != "" do
+      HTTPoison.post(
+        aligator_url() <> "/conversation",
+        %{"event" => payload} |> Poison.encode!,
+        [
+          {"Content-Type", "application/json"},
+        ]
+      )
+    else
+      nil
+    end
+  end
+
+  defp aligator_url() do
+    Application.get_env(:shooter, Aligator, [])
+    |> Keyword.get(:aligator_url, "")
+  end
 end
