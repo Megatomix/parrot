5bd01ff9c0e7b716c13441e1142eae3f35c8feaa
Gustavo Aguiar <gugahoa@gmail.com>
2018-03-29 14:20:25 +0000

    Send to integration and broadcast to others

--- apps/parrot_web/lib/parrot_web/channels/room_channel.ex
+++ apps/parrot_web/lib/parrot_web/channels/room_channel.ex
@@ -1,8 +1,11 @@
 defmodule ParrotWeb.RoomChannel do
   use ParrotWeb, :channel
+
+  require Logger
+
   intercept ["new_event"]
 
-  def join("room:" <> _userId, payload, socket) do
+  def join("room:" <> _appId, payload, socket) do
     if authorized?(payload) do
       {:ok, socket}
     else
@@ -16,37 +19,35 @@ defmodule ParrotWeb.RoomChannel do
     {:reply, {:ok, payload}, socket}
   end
 
-  # It is also common to receive messages from the client and
-  # broadcast to everyone in the current topic (room:lobby).
-  def handle_in("shout", payload, socket) do
-    broadcast socket, "shout", payload
+  def handle_in("new_event", payload, socket) do
+    broadcast_msg(payload, socket)
+
+    Shooter.shoot_msg(payload)
     {:noreply, socket}
   end
 
-  def handle_in("new_event", %{"type" => "MSG"} = payload, socket) do
-    new_payload = Map.put(payload, "type", "RECV")
-    broadcast socket, "new_event", new_payload
+  def handle_out("new_event",
+                 %{"app_id" => app_id,
+                   "user_id" => user_id
+                 } = payload,
+                 %{assigns: %{app_id: app_id, user_id: user_id}} = socket) do
+    push(socket, "new_event", payload)
     {:noreply, socket}
   end
 
-  def handle_in("new_event", _payload, socket) do
-    {:noreply, socket}
-  end
-
-  def handle_out("new_event", %{"app_id" => app_id} = payload, %{assigns: %{app_id: app_id}} = socket) do
-    require Logger
-    Logger.debug inspect(payload)
-    Logger.debug inspect(socket.assigns)
-    push socket, "new_event", payload
-    {:noreply, socket}
-  end
-
-  def handle_out("new_event", payload, socket) do
+  def handle_out("new_event", _payload, socket) do
     {:noreply, socket}
   end
 
   # Add authorization logic here as required.
-  defp authorized?(_payload) do
+  defp authorized?(payload) do
     true
   end
+
+  defp broadcast_msg(%{"type" => "MSG"} = payload, socket) do
+    new_payload = Map.put(payload, "type", "RECV")
+
+    broadcast_from(socket, "new_event", new_payload)
+  end
+  defp broadcast_msg(_payload, _socket), do: nil
 end
