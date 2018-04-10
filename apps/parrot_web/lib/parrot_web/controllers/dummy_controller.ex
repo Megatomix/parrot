60a6fe5d52467058d92b1f1fc44ce22c102b8587
Gustavo Aguiar <gugahoa@gmail.com>
2018-04-10 20:24:47 +0000

    Send to Aligator every incoming message

--- apps/parrot_web/lib/parrot_web/controllers/dummy_controller.ex
+++ apps/parrot_web/lib/parrot_web/controllers/dummy_controller.ex
@@ -10,17 +10,19 @@ defmodule ParrotWeb.DummyController do
     ParrotWeb.Endpoint.broadcast("room:#{params["app_id"]}", "new_event", params)
 
     Task.async(fn ->
-      echo_back(params)
+      handle_in(params)
     end)
 
     conn
     |> send_resp(200, "Ok")
   end
 
-  defp echo_back(%{"type" => "RECV"} = payload) do
+  defp handle_in(%{"type" => "RECV"} = payload) do
     echo = Map.put(payload, "type", "ECHO")
 
     Shooter.shoot_msg(echo)
   end
-  defp echo_back(_payload), do: nil
+  defp handle_in(payload) do
+    Shooter.shoot_msg(payload)
+  end
 end
